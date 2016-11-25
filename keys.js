b7bd7d49e9922dcf29e0ab176204bd831d9bdb28
tomtom28 <tomthompson828@gmail.com>
2016-11-25 17:17:23 -0500

    added a readme and photos

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 //console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 'paste-here',
-  consumer_secret: 'paste-here',
-  access_token_key: 'paste-here',
-  access_token_secret: 'paste-here'
+  consumer_key: 'dFIe3FKqOo8WNGWTIhOFcuwrF',
+  consumer_secret: 'unzbaaItSla4i2Roid9dRSQElgfSvbRKOKYwZSNmaYFlXzLyMt',
+  access_token_key: '757732018201518080-KPtbWRrmlEYyKxTgy5YUsPidiy2BOLs',
+  access_token_secret: 'rjfI13A2qSDFjD6uX9z5r6MR9kXaCoH7BBGipltMw4lNA'
 }
